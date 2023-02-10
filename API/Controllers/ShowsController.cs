8e5493c6330ccfff52900ed53ee2fbf778410b2e
Leticia Cruz Menezes <leticmz@gmail.com>
2023-02-09 21:56:09 -0300

    ADD: initial files for api development using .net core 6

---
+++ API/Controllers/ShowsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShowsAPI.Controllers
+{
+    public class ShowsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
